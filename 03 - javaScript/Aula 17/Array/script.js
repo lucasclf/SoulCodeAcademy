5b49b428ca95248491a75ec3db2bb34c16da0e73
lucasclf <cqn.lucas@gmail.com>
2021-08-10 10:42:51 -0300

    Atualização dos arquivos de aprendizado.

---
+++ 03 - javaScript/Aula 17/Array/script.js
@@ -0,0 +1,15 @@
+var frutas = ["banana", "manga", "pera", "jaca"], legumes = [];
+var pessoa = {"nome":"Sayure", "endereço":"Rua b", "idade":15};
+
+for (i = 0; i < 4; i++) {
+    legumes[i] = prompt("Digite um legumes: ")
+}
+
+
+
+console.log("frutas: ", frutas)
+console.log("legumes: ", legumes)
+console.log("pessoa:", pessoa)
+
+
+
