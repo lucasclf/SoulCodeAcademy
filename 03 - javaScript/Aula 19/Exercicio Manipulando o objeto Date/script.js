3568a6b810a0b3ae5a17ef366dba37acb248d94c
lucasclf <cqn.lucas@gmail.com>
2021-08-12 13:23:50 -0300

    Exercicio Manipulando datas

---
+++ 03 - javaScript/Aula 19/Exercicio Manipulando o objeto Date/script.js
@@ -0,0 +1,45 @@
+// QUESTÃO 01:
+
+/* var txt = "abcdefghijklm"
+var pos = txt.indexOf("h")
+document.write(pos) */
+
+// QUESTÃO 02:
+
+/* var txt = "Posso comer bananas o dia todo"
+var pos = txt.slice(txt.indexOf(" b"), txt.indexOf(" o"))
+document.write(pos) */
+
+// QUESTÃO 03:
+
+/* var txt = "Olá, mundo"
+txt = txt.replace = "Olá, bem-vindo"
+document.write(txt)  */
+
+// QUESTÃO 04:
+
+/* var txt = "Olá, mundo"
+txt = txt.toUpperCase()
+document.write(txt) */
+
+// QUESTÃO 05:
+
+/* var txt = "OLÁ, MUNDO"
+txt = txt.toLowerCase()
+ */
+
+// QUESTÃO 06:
+
+/* var txt = "Olá, mundo!"
+x = txt.length
+alert(x) */
+
+// QUESTÃO 07:
+
+/* var txt = "We are \"Vikings\""
+alert(txt) */
+
+// QUESTÃO 08:
+
+/* var str1 = "Olá ", str2 = "Mundo!"
+alert(str1+str2) */
