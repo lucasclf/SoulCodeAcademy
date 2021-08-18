54ba9173fd377dbf89cc114109e00873eb0316cc
lucasclf <cqn.lucas@gmail.com>
2021-08-18 10:59:55 -0300

    .

---
+++ 04 - jQuery/Aula 23/Posicionamento scroll dimensionamento/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+//Questão 01
+$("div").height(500).width(500)
+//Questão 02
+console.log($("div").height())
+//Questão 03
+console.log($("div").innerHeight())
+})
---
+++ 04 - jQuery/Aula 23/Scroll/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+      $("p").clone().appendTo(document.body);
+      $("p").clone().appendTo(document.body);
+      $("p").clone().appendTo(document.body);
+      $(window).scroll(function () {
+        $("span").css("display", "inline").fadeOut("slow");
+      });
+})
