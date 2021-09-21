0a28695390713ff2ab9ce6878403ccad47e99f2b
lucasclf <cqn.lucas@gmail.com>
2021-09-21 09:14:26 -0300

    Alteração de nome.

--- 06 - BancoDados/Geral/vendas/connect.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require("mongoose")
-
-mongoose.connect("mongodb+srv://lucas_cqn:lucas_cqn@cluster0.xn84s.mongodb.net/vendas?retryWrites=true&w=majority").then(() => {
-    console.log("banco conectado!")
-}).catch((err) => {
-    console.log("Deu ruim!" + err)
-})
