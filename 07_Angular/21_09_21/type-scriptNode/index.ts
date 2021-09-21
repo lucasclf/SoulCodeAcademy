14201b2ab06035ba08b538fa17704c47f3ef762f
lucasclf <cqn.lucas@gmail.com>
2021-09-21 10:24:59 -0300

    teste

---
+++ 07_Angular/21_09_21/type-scriptNode/index.ts
@@ -0,0 +1,54 @@
+class Pessoa{
+
+    constructor(private nome:string, private idade:number, private cpf:string){
+        this.nome = nome
+        this.idade = idade
+        this.cpf = cpf
+    }
+
+    escreverPropriedades(){
+        console.log(this.nome)
+        console.log(this.idade)
+        console.log(this.cpf)
+    }
+
+    getNome(){
+        return this.nome;
+    }
+
+    setNome(nome: string){
+        this.nome = nome;
+    }
+
+    getIdade(){
+        return this.idade;
+    }
+
+    setIdade(idade: number){
+        this.idade = idade;
+    }
+
+    getCpf(){
+        return this.cpf;
+    }
+
+    setCpf(Cpf: string){
+        this.cpf = Cpf;
+    }
+
+}
+
+let pessoa = new Pessoa("Lucas", 34, "111.111.111-11")
+
+/* console.log(pessoa.escreverPropriedades()) */
+
+pessoa.setNome("Pedro")
+console.log(pessoa.getNome())
+
+interface propriedadesCarro{
+    marca: private string
+}
+
+class Carro implements propriedadesCarro{
+    marca!: string
+}
